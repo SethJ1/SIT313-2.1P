29b5f353bf3879e0cecc09a6c1eab319c4a27b61
unknown <sethj4774@gmail.com>
2023-07-28 14:10:04 +1000

    Finished working code

--- server.js
+++ server.js
@@ -1,45 +1,46 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
 const app = express();
 
-require("dotenv").config();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const sgMail = require("sendgrid/mail");
+const apiKey = '4de184ba842d30748546843685231caf-73f745ed-640731c2';
+const domain = 'sandboxc0a8a00bbf0745d390c4ee76d1d5f749.mailgun.org';
+const mailgun = require('mailgun-js')({ apiKey, domain });
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+app.use(express.static('public'));
 
-const sendMail = async (msg) => {
-    try{
-        await sgMail.send(msg);
-        console.log("Message sent successfully!");
+app.post('/signup', (req, res) => {
+  const email = req.body.email;
+  if (!email) {
+    return res.status(400).json({ error: 'Email is required!' });
+  }
+
+  const data = {
+    from: 'sethj4774@gmail.com',
+    to: email,
+    subject: 'Hello',
+    text: 'Testing some Mailgun awesomeness!'
+  };
+
+  mailgun.messages().send(data, (error, body) => {
+    if (error) {
+      console.log(error);
+      return res.status(500).json({ error: 'An error occurred while sending the email.' });
+    } else {
+      console.log(body);
+      return res.status(200).json({ message: 'Email sent successfully!' });
     }
-    catch (error) {
-        console.error(error);
-
-        if (error.response) {
-            console.error(error.response.body);
-        }
-    }
-};
-
-app.get('/', (req, res)=>{
-    res.sendfile(__dirname + "/index.html")
-})
-
-app.post('/', (req, res) =>{
-    const email = req.body.email;
-
-    res.send("Email submitted");
-
-    sendMail({
-        to: email,
-        from: "sethj4774@gmail.com",
-        subject: "Welcome Message",
-        text: "Welcome, thanks for joining!",
-    });
-
+  });
 });
 
-app.listen(3000, function (request, response){
-    console.log("Server is running")
-})
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  });
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
+
+
